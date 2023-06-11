dce3c86fc9bba13f19b3b54db7d3e7cf520a2872
Saptarshi2446 <112680182+Saptarshi2446@users.noreply.github.com>
2023-06-12 00:08:58 +0530

    Update test.py V2.0

--- test.py
+++ test.py
@@ -1,32 +1,28 @@
+import argparse
 import requests
 import json
-import glob
 
 ZABBIX_API_URL = "http://3.21.37.213/zabbix/api_jsonrpc.php"
 UNAME = "Admin"
 PWORD = "zabbix"
 
-# Login to Zabbix API and obtain authentication token
-r = requests.post(ZABBIX_API_URL, json={
-    "jsonrpc": "2.0",
-    "method": "user.login",
-    "params": {
-        "user": UNAME,
-        "password": PWORD
-    },
-    "id": 1
-})
-AUTHTOKEN = r.json()["result"]
+def import_yaml_file(file_path):
+    # Login to Zabbix API and obtain authentication token
+    r = requests.post(ZABBIX_API_URL, json={
+        "jsonrpc": "2.0",
+        "method": "user.login",
+        "params": {
+            "user": UNAME,
+            "password": PWORD
+        },
+        "id": 1
+    })
+    AUTHTOKEN = r.json()["result"]
 
-# Find all YAML files in the directory
-yaml_files = glob.glob("*.yaml")
-
-# Iterate over each YAML file and perform import operation
-for yaml_file in yaml_files:
-    with open(yaml_file, 'r') as file:
+    with open(file_path, 'r') as file:
         yml_data = file.read()
 
-    # Perform import request for the current YAML file
+    # Perform import request for the YAML file
     r = requests.post(ZABBIX_API_URL, json={
         "jsonrpc": "2.0",
         "method": "configuration.import",
@@ -58,18 +54,25 @@ for yaml_file in yaml_files:
         "auth": AUTHTOKEN
     })
 
-    # Print the import response for the current YAML file
-    print(f"Import response for '{yaml_file}':")
+    # Print the import response for the YAML file
+    print(f"Import response for '{file_path}':")
     print(json.dumps(r.json(), indent=4, sort_keys=True))
     print()
 
-# Logout user
-r = requests.post(ZABBIX_API_URL, json={
-    "jsonrpc": "2.0",
-    "method": "user.logout",
-    "params": {},
-    "id": 3,
-    "auth": AUTHTOKEN
-})
-print("Logout response:")
-print(json.dumps(r.json(), indent=4, sort_keys=True))
+    # Logout user
+    r = requests.post(ZABBIX_API_URL, json={
+        "jsonrpc": "2.0",
+        "method": "user.logout",
+        "params": {},
+        "id": 3,
+        "auth": AUTHTOKEN
+    })
+    print("Logout response:")
+    print(json.dumps(r.json(), indent=4, sort_keys=True))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Import a YAML file to Zabbix")
+    parser.add_argument("file_path", help="Path to the YAML file to be imported")
+    args = parser.parse_args()
+    import_yaml_file(args.file_path)
+
